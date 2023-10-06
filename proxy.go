07338d7f87b05508158649b704a54f0a11b42bae
bmf-san <bmf.infomation@gmail.com>
2023-10-07 01:10:21 +0900

    [fix]

--- proxy.go
+++ proxy.go
@@ -7,7 +7,7 @@ import (
 )
 
 func New() {
-	backend, _ := url.Parse("http://localhost:8081")
+	backend, _ := url.Parse("http://backend:8081")
 	proxy := httputil.NewSingleHostReverseProxy(backend)
 	http.Handle("/", proxy)
 	http.ListenAndServe(":8080", nil)
