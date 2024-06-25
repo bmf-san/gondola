c442bddf05f840269ef17209dab4779e3b585d05
bmf-san <bmf.infomation@gmail.com>
2024-06-25 20:30:05 +0900

    [update] improved test coverage

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log/slog"
 	"os"
@@ -17,6 +18,31 @@ func init() {
 
 }
 
+// parseFlags parses the command line flags.
+func parseFlags() error {
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		flag.Usage()
+		return errors.New("config file is not specified")
+	}
+	return nil
+}
+
+// setConfig returns the config file.
+func setConfig(cfgFile string) (*os.File, error) {
+	if cfgFile == "" {
+		return nil, errors.New("config file is not specified")
+	}
+
+	cfg, err := os.Open(filepath.Clean(cfgFile))
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
 func main() {
 	defer func() {
 		if x := recover(); x != nil {
@@ -25,31 +51,17 @@ func main() {
 		os.Exit(1)
 	}()
 
-	flag.Parse()
-
-	if len(os.Args) == 1 {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	if cfgFile == "" {
-		slog.Error("config file is not specified")
-		os.Exit(1)
-	}
-
-	cfg, err := os.Open(filepath.Clean(cfgFile))
+	err := parseFlags()
 	if err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
 
-	defer func() {
-		err = cfg.Close()
-		if err != nil {
-			slog.Error(err.Error())
-			os.Exit(1)
-		}
-	}()
+	cfg, err := setConfig(cfgFile)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
 
 	gondola, err := gondola.NewGondola(cfg)
 	if err != nil {
